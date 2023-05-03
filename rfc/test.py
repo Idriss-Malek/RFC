ad6f1ac332171c284902bc1749de2aa1f3d78811
Idriss Malek <idrissmalek80@gmail.com>
2023-05-03 10:29:54 -0400

    bug detection

--- rfc/test.py
+++ rfc/test.py
@@ -6,17 +6,16 @@ from rfc.module import TreeEnsembleCompressor, TreeEnsembleCompressorStatus
 if __name__ == '__main__':
     import pathlib
     root = pathlib.Path(__file__).parent.resolve().parent.resolve() / 'resources'
-    dataset = root / 'datasets/FICO/FICO.full.csv'
-    ensemble = root / 'forests/FICO/FICO.RF1.txt'
+    dataset = root / 'datasets/Pima-Diabetes/Pima-Diabetes.full.csv'
+    ensemble = root / 'forests/Pima-Diabetes/Pima-Diabetes.RF1.txt'
     dataset = str(dataset)
     dataset = pd.read_csv(dataset)
     ensemble = str(ensemble)
     ensemble = load_tree_ensemble(ensemble, log_output=False)
     cmp = TreeEnsembleCompressor(ensemble, dataset, lazy=True)
-    cmp.compress(on='full', log_output=True, precision=8, m_iterations=10)
-    if cmp.status != TreeEnsembleCompressorStatus.OPTIMAL:
-        print('Solver did not find any solution.')
-    elif check_on_dataset(ensemble, cmp.sol, dataset):
-        print('Compression successful.')
-    else:
-        print('Compression failed.')
+    cmp.compress(on='train', log_output=True, precision=8, m_iterations=10)
+    res=cmp.sep.separate(cmp.sol)
+    x=res[(0,1)]
+    print('NEW CLASS : ',ensemble.klass(x,list(cmp.sol)))
+    print('OLD CLASS : ', ensemble.klass(x))   
+    
