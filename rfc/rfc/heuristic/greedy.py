407a43c2c9eea1d92c28ec58c48ceef66d79fdbf
Idriss Malek <idrissmalek80@gmail.com>
2023-07-15 16:32:51 -0400

    greedy

--- rfc/rfc/heuristic/greedy.py
+++ rfc/rfc/heuristic/greedy.py
@@ -12,23 +12,45 @@ class GreedyCompressor:
     def __init__(self, ensemble: Ensemble, dataset: pd.DataFrame) -> None:
         self.ensemble = ensemble
         self.dataset = dataset
+        self.u = np.ones(len(self.ensemble))
+        self.p = np.vstack(tuple(self.dataset.apply(self.ensemble.p, axis=1).values))
 
-    def solve(self):
-        p = self.dataset.apply(self.ensemble.p, axis=1).values
-        p = np.vstack(tuple(p))
-        k = np.argmax(p, axis=1)
-        idx = list(range(len(self.ensemble)))
-        random.shuffle(idx)
-        r = []
-        for t in idx:
-            T = self.ensemble[t]
-            pp = []
-            for c in range(self.ensemble.n_classes):
-                ppp = self.dataset.apply(lambda x: T.F(x, c), axis=1).values
-                pp.append(ppp)
-            pp = np.vstack(tuple(pp)).T
-            kk = np.argmax(p - pp, axis=1)
-            if (k == kk).all():
-                r.append(t)
-                p -= pp
-        print(len(r))
+    def solve(self,iterations=100, tuples=1, choices = None):
+        if tuples<1:
+            raise TypeError('tuples has to be a postive integer.')
+        if choices is None:
+            choices = int(len(self.ensemble)**1.5)
+        for _ in range(iterations):
+            u = np.ones(len(self.ensemble))
+            p = self.p.copy()
+            k = np.argmax(p, axis=1)
+            idx = list(range(len(self.ensemble)))
+            random.shuffle(idx)
+            for i in range(tuples,1,-1):
+                for choice in range(choices):
+                    couple = np.random.choice(np.nonzero(self.u), i, replace = False)
+                    T=[self.ensemble[t] for t in couple]
+                    pp = []
+                    for c in range(self.ensemble.n_classes):
+                        ppp = self.dataset.apply(lambda x: sum([t.F(x, c) for t in T]), axis=1).values#type:ignore
+                        pp.append(ppp)
+                    pp = np.vstack(tuple(pp)).T
+                    kk = np.argmax(p - pp, axis=1)
+                    if (k == kk).all():
+                        for t in couple:
+                            u[t] = 0.
+                            p -= pp
+                    
+            for t in idx:
+                T = self.ensemble[t]
+                pp = []
+                for c in range(self.ensemble.n_classes):
+                    ppp = self.dataset.apply(lambda x: T.F(x, c), axis=1).values #type:ignore
+                    pp.append(ppp)
+                pp = np.vstack(tuple(pp)).T
+                kk = np.argmax(p - pp, axis=1)
+                if (k == kk).all():
+                    u[t] = 0.
+                    p -= pp
+                if sum(u) < sum(self.u):
+                    self.u = u
--- rfc/test_greedy.py
+++ rfc/test_greedy.py
@@ -1,21 +1,40 @@
 from rfc.heuristic import GreedyCompressor
 import pathlib
 import pandas as pd
+import time
 
 from rfc.utils import load_tree_ensemble
 
 if __name__ == '__main__':
     root = pathlib.Path(__file__).parent.resolve().parent.resolve() / 'resources'
-    for i in range (1,2):
-        dataset = root / f'datasets/Pima-Diabetes/Pima-Diabetes.train{i}.csv'
-        test_dataset = root / f'datasets/Pima-Diabetes/Pima-Diabetes.test{i}.csv'
-        ensemble = root / f'forests/Pima-Diabetes/Pima-Diabetes.RF{i}.txt'
-        dataset = str(dataset)
-        dataset = pd.read_csv(dataset)
-        test_dataset = str(test_dataset)
-        test_dataset = pd.read_csv(test_dataset)
-        ensemble = str(ensemble)
-        ensemble = load_tree_ensemble(ensemble, log_output=False)
-        greedy = GreedyCompressor(ensemble, dataset)
-        greedy.solve()
+    comp = lambda x,y : x<y
+    with open('test_greedy.csv', 'a+') as f:
+        f.write(f'Ensemble,Tuple size, Original size, Compressed size,  Original accuracy on testset , Compressed accuracy on testset, time\n')
+
+    for word in ['FICO', 'HTRU2','Pima-Diabetes','COMPAS-ProPublica']:        
+        for i in range (1,11):
+            dataset = root / f'datasets/{word}/{word}.train{i}.csv'
+            test_dataset = root / f'datasets/{word}/{word}.test{i}.csv'
+            ensemble = root / f'forests/{word}/{word}.RF{i}.txt'
+            dataset = str(dataset)
+            dataset = pd.read_csv(dataset)
+            test_dataset = str(test_dataset)
+            test_dataset = pd.read_csv(test_dataset)
+            ensemble = str(ensemble)
+            ensemble = load_tree_ensemble(ensemble, log_output=False)
+            greedy = GreedyCompressor(ensemble, dataset)
+            for j in range(1,3):
+                t1 = time.time()
+                greedy.solve(iterations=100,tuples=j)
+                t2 = time.time()
+                acc1=0
+                acc2=0
+                for _,row in test_dataset.iterrows():
+                    acc1 += (ensemble.klass(row,tiebreaker = comp) == row[-1]) +0. #type:ignore
+                    acc2 += (ensemble.klass(row, greedy.u, tiebreaker = comp) == row[-1]) +0. #type:ignore
+                acc1 /= len(test_dataset)
+                acc2 /= len(test_dataset)
+                
+                with open('test_greedy.csv', 'a+') as f:
+                    f.write(f'{word} {i}, {j}, {len(greedy.u)}, {sum(greedy.u)},{acc1} ,{acc2}, {t2 - t1} \n')
         
